6147bd425b3bde5c8279d95d123a6d1831bfd3e9
MM-Lium <63448518+MM-Lium@users.noreply.github.com>
2021-11-30 18:40:56 +0800

    1130

---
+++ myweb/ex/samsung.js
@@ -0,0 +1,38 @@
+function script(){
+
+
+	let $body = document.body;
+	let winH = window.innerHeight;
+	let winW = window.innerWidth;
+
+	let $win = window;
+
+
+	console.log(winH);
+	console.log("aaa");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+script();
