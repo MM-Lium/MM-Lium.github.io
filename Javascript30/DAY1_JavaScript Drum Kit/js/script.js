23d9293a1d1037bcb964b5cc8809b43730fb280f
MM-Lium <63448518+MM-Lium@users.noreply.github.com>
2021-12-15 16:54:03 +0800

    1215

--- Javascript30/DAY1_JavaScript Drum Kit/js/script.js
+++ Javascript30/DAY1_JavaScript Drum Kit/js/script.js
@@ -3,32 +3,38 @@ var $win = $(window),
     winH = $win.height(),
     _media = 1280;
 
-var myScript = function() {
+var myScript = function () {
 
-    $(document).ready(function() {
+        window.addEventListener('keydown', function (e) {
 
-        window.addEventListener('keydown',function(e){
-           
 
             const audio = document.querySelector(`li[data-key="${e.keyCode}"]`).dataset.src;
-            const musiz = "sounds/"+ audio +".wav";
+            const key = document.querySelector(`li[data-key="${e.keyCode}"]`);
+            const musiz = "sounds/" + audio + ".wav";
             const audioPlayer = document.createElement('audio');
 
-            audioPlayer.setAttribute('src',musiz);
+            audioPlayer.setAttribute('src', musiz);
             audioPlayer.play();
+            key.classList.add('play');
 
-        })
+        });
+
+        function removett(e) {
+            if (e.propertyName !== 'transform') return;
+
+            this.classList.remove('play');
+        }
+
+        const keys = document.querySelectorAll('.btn_p');
+        keys.forEach(key => key.addEventListener("transitionend",removett));
 
 
-
-
-    });
 
 }
 
 
 /*------------JQ----------------------------------------- */
-$(function() {
+$(function () {
 
     var main = myScript();
 
