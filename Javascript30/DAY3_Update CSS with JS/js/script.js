464183e593e668833e72ad5c6ed3f1d3920a300a
MM-Lium <63448518+MM-Lium@users.noreply.github.com>
2022-02-15 15:20:35 +0800

    2/15

--- Javascript30/DAY3_Update CSS with JS/js/script.js
+++ Javascript30/DAY3_Update CSS with JS/js/script.js
@@ -5,37 +5,7 @@ var $win = $(window),
 
 var myScript = function () {
 
-    var _transform = 0;
 
-
-    $('.clock .number li').each(function () {
-        var _all = $(this).index();
-        $(this).css('transform', 'rotate(' + _all * 30 + 'deg) ');
-    })
-
-    const seconhand = document.querySelector('.second');
-
-
-    function setdate() {
-        const now = new Date();
-
-        const seconds = now.getSeconds();
-        const gosec = seconds * 6 + 90;
-
-        const min = now.getMinutes();
-        const gomin = min * 6 + 90;
-
-        const hour = now.getHours();
-        const gohour = hour * 30 + 90;
-
-        // $('.clock .second').css('transform', 'rotate(' + gosec + 'deg) ');
-        seconhand.style.transform=`rotate(${gosec}deg)`;
-        
-        $('.clock .minute').css('transform', 'rotate(' + gomin + 'deg) ');
-        $('.clock .hour').css('transform', 'rotate(' + gohour + 'deg) ');
-    }
-
-    setInterval(setdate, 1000);
 }
 
 /*------------JQ----------------------------------------- */
