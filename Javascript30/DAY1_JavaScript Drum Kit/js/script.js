27bac0aded7045328bc87358df9e7c06f6562c7d
MM-Lium <63448518+MM-Lium@users.noreply.github.com>
2022-05-12 18:23:43 +0800

    5/12

--- Javascript30/DAY1_JavaScript Drum Kit/js/script.js
+++ Javascript30/DAY1_JavaScript Drum Kit/js/script.js
@@ -5,27 +5,27 @@ var $win = $(window),
 
 var myScript = function () {
 
-        window.addEventListener('keydown', function (e) {
-            const audio = document.querySelector(`li[data-key="${e.keyCode}"]`).dataset.src;
-            const key = document.querySelector(`li[data-key="${e.keyCode}"]`);
-            const musiz = "sounds/" + audio + ".wav";
-            const audioPlayer = document.createElement('audio');
+    window.addEventListener('keydown', function (e) {
+        const audio = document.querySelector(`li[data-key="${e.keyCode}"]`).dataset.src;
+        const key = document.querySelector(`li[data-key="${e.keyCode}"]`);
+        const musiz = "sounds/" + audio + ".wav";
+        const audioPlayer = document.createElement('audio');
 
-            audioPlayer.setAttribute('src', musiz);
-            audioPlayer.play();
-            key.classList.add('play');
+        audioPlayer.setAttribute('src', musiz);
+        audioPlayer.play();
+        audioPlayer.currentTime = 0;
+        key.classList.add('play');
 
-        });
+    });
 
-        function removett(e) {
-            if (e.propertyName !== 'transform') return;
-            this.classList.remove('play');
-
-            console.log(e);
+    function removett(e) {
+        if (e.propertyName === 'transform') {
+            e.currentTarget.classList.remove('play');
         }
-        
-        const keys = document.querySelectorAll('.btn_p');
-        keys.forEach(key => key.addEventListener("transitionend",removett));
+    }
+
+    const keys = document.querySelectorAll('.btn_p');
+    keys.forEach(key => key.addEventListener("transitionend", removett));
 }
 
 
--- Javascript30/DAY3_Update CSS with JS/js/script.js
+++ Javascript30/DAY3_Update CSS with JS/js/script.js
@@ -9,69 +9,34 @@ var myScript = function () {
     let startY = 0;
 
 
-    $('.line').mousemove(function (e) {
-        startX = e.clientX - $(this).offset().left;
-        startY = e.clientY - $(this).offset().top;
-        x = startX;
-        y = startY;
-        let _tag = $(this).data('tag');
-        if (x <= "300") {
-            $(this).find('.bull').css({
-                left: startX,
-                // top: y
-            });
-            if (_tag == "spa") {
-                $(this).prev().css('padding-left', startX);
-            } else if (_tag == "blu") {
-                $(this).prev().css('filter', 'blur(' + startX + 'px)');
-            } else if (_tag == "color") {}
-        } else {
-            return false;
-        }
-    });
+    // $('.line').mousemove(function (e) {
+    //     startX = e.clientX - $(this).offset().left;
+    //     startY = e.clientY - $(this).offset().top;
+    //     x = startX;
+    //     y = startY;
+    //     let _tag = $(this).data('tag');
+    //     if (x <= "300") {
+    //         $(this).find('.bull').css({
+    //             left: startX,
+    //             // top: y
+    //         });
+    //         if (_tag == "spa") {
+    //             $(this).prev().css('padding-left', startX);
+    //         } else if (_tag == "blu") {
+    //             $(this).prev().css('filter', 'blur(' + startX + 'px)');
+    //         } else if (_tag == "color") {}
+    //     } else {
+    //         return false;
+    //     }
+    // });
 
     // const inputs = document.querySelectorAll('.control input');
-
     // function handle() {
     //     const _num = this.dataset.size || '';
     //     document.documentElement.style.setProperty(`--${this.name}`, this.value);
     // }
-
     // inputs.forEach(input => input.addEventListener('change', handle));
     // inputs.forEach(input => input.addEventListener('mousemove', handle));
-
-    var arr = [10, 1, 11, 85, 6, 3, 50];
-
-
-    var i = j = text = 0;
-
-    for (i = 1; i < arr.length; i++) {
-
-        text = arr[i];
-        console.log(i);
-        
-
-        for (j = i - 1; j >= 0; j--) {
-            console.log(i);
-
-            if (text > arr[j]) {
-                console.log(arr[j]);
-                break;
-            } else {
-                arr[j + 1] = arr[j];
-            }
-        }
-        arr[j + 1] = text;
-
-
-
-    }
-
-
-    console.log(arr);
-
-
-
 
 }
 
